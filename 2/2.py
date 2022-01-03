d70d8528cb4dce600ce82e5a418ad68483ec52f3
t109598087@ntut.org.tw <t109598087@ntut.org.tw>
2022-01-04 02:03:20 +0800

    2 start ok

--- 2/2.py
+++ 2/2.py
@@ -1,18 +1,22 @@
 from itertools import product, permutations
 
-M = int(input())
-N = int(input())
-print(list(permutations([0, 1, 2, 3, 4])))
+row1_split = input().split(' ')
+M = int(row1_split[0])
+N = int(row1_split[1])
+
 classroom_dict = dict()
 for _ in range(M):
     classroom_split = input().split(' ')
-    classroom_dict[classroom_split[0]] = classroom_split[1]
+    classroom_dict[classroom_split[0]] = int(classroom_split[1])
 print(classroom_dict)
-
 
 class_dict = dict()
 for _ in range(N):
     class_split = input().split(' ')
-    class_dict[class_split[0]] = [class_split[1], class_split[2], class_split[3]]
+    class_dict[class_split[0]] = [int(class_split[1]), int(class_split[2]), int(class_split[3])]
 
 print(class_dict)
+
+all_comb = list(permutations([i for i in range(N)]))
+print(all_comb)
+
