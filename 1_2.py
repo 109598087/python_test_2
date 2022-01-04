a75f4340f8973ce96724b78b1753bc547980b5be
109598087 <t109598087@ntut.org.tw>
2022-01-04 12:35:18 +0800

    1 ok

--- 1_2.py
+++ 1_2.py
@@ -17,6 +17,9 @@ def computer_go(start_game):
         temp_game = into_start_game(num, temp_game, 1)
         if get_result(temp_game) == 1:
             return num
+    live_two_position_list = get_live_two_position(start_game)
+    if len(live_two_position_list) == 1:
+        return live_two_position_list[0]
     # good choice
     good_list = [5, 1, 3, 7, 9]
     for num in good_list:
@@ -25,6 +28,73 @@ def computer_go(start_game):
     # random choice
     for num in index_0_list:
         return num
+
+
+def get_live_two_position(start_game):
+    index_0_list = get_0_index(start_game)
+    num_list = list()
+    for num in index_0_list:
+        temp_game = copy.deepcopy(start_game)
+        temp_game = into_start_game(num, temp_game, 1)
+        count = 0
+        if temp_game[0][0] == temp_game[0][1] == 1 and temp_game[0][2] == 0:
+            count += 1
+        if temp_game[1][0] == temp_game[1][1] == 1 and temp_game[1][2] == 0:
+            count += 1
+        if temp_game[2][0] == temp_game[2][1] == 1 and temp_game[2][2] == 0:
+            count += 1
+
+        if temp_game[0][0] == temp_game[1][0] == 1 and temp_game[2][0] == 0:
+            count += 1
+        if temp_game[0][1] == temp_game[1][1] == 1 and temp_game[2][1] == 0:
+            count += 1
+        if temp_game[0][2] == temp_game[1][2] == 1 and temp_game[2][2] == 0:
+            count += 1
+
+        if temp_game[0][0] == temp_game[0][2] == 1 and temp_game[0][1] == 0:
+            count += 1
+        if temp_game[1][0] == temp_game[1][2] == 1 and temp_game[1][1] == 0:
+            count += 1
+        if temp_game[2][0] == temp_game[2][2] == 1 and temp_game[2][1] == 0:
+            count += 1
+
+        if temp_game[0][0] == temp_game[2][0] == 1 and temp_game[1][0] == 0:
+            count += 1
+        if temp_game[0][1] == temp_game[2][1] == 1 and temp_game[1][1] == 0:
+            count += 1
+        if temp_game[0][2] == temp_game[2][2] == 1 and temp_game[1][2] == 0:
+            count += 1
+
+        if temp_game[0][2] == temp_game[0][1] == 1 and temp_game[0][0] == 0:
+            count += 1
+        if temp_game[1][2] == temp_game[1][1] == 1 and temp_game[1][0] == 0:
+            count += 1
+        if temp_game[2][2] == temp_game[2][1] == 1 and temp_game[2][0] == 0:
+            count += 1
+
+        if temp_game[2][0] == temp_game[1][0] == 1 and temp_game[0][0] == 0:
+            count += 1
+        if temp_game[2][1] == temp_game[1][1] == 1 and temp_game[0][1] == 0:
+            count += 1
+        if temp_game[2][2] == temp_game[1][2] == 1 and temp_game[0][2] == 0:
+            count += 1
+
+        if temp_game[0][0] == temp_game[1][1] == 1 and temp_game[2][2] == 0:
+            count += 1
+        if temp_game[0][0] == temp_game[2][2] == 1 and temp_game[1][1] == 0:
+            count += 1
+        if temp_game[2][2] == temp_game[1][1] == 1 and temp_game[0][0] == 0:
+            count += 1
+
+        if temp_game[0][2] == temp_game[1][1] == 1 and temp_game[2][0] == 0:
+            count += 1
+        if temp_game[0][2] == temp_game[2][0] == 1 and temp_game[1][1] == 0:
+            count += 1
+        if temp_game[2][0] == temp_game[1][1] == 1 and temp_game[0][2] == 0:
+            count += 1
+        if count == 2:
+            num_list.append(num)
+    return num_list
 
 
 M = int(input())
--- 2/2.py
+++ 2/2.py
@@ -1,22 +1,35 @@
-from itertools import product, permutations
+# from itertools import product, permutations, combinations
+#
+# row1_split = input().split(' ')
+# M = int(row1_split[0])
+# N = int(row1_split[1])
+#
+# classroom_dict = dict()
+# for _ in range(M):
+#     classroom_split = input().split(' ')
+#     classroom_dict[classroom_split[0]] = int(classroom_split[1])
+# print(classroom_dict)
+#
+# class_dict = dict()
+# for _ in range(N):
+#     class_split = input().split(' ')
+#     class_dict[class_split[0]] = [int(class_split[1]), int(class_split[2]), int(class_split[3])]
+# print(class_dict)
+#
+# class_permutation = list(permutations([key for key in class_dict.keys()]))
+# print(class_permutation)
+# classroom_time_dict = {key: [i + 1 for i in range(24)] for key in classroom_dict.keys()}
+#
+# print(list(combinations(classroom_dict.keys(), 2)))
+from itertools import combinations, permutations
 
-row1_split = input().split(' ')
-M = int(row1_split[0])
-N = int(row1_split[1])
+from itertools import combinations
 
-classroom_dict = dict()
-for _ in range(M):
-    classroom_split = input().split(' ')
-    classroom_dict[classroom_split[0]] = int(classroom_split[1])
-print(classroom_dict)
+print(set(list(permutations([1102, 1102, 1102, 2103], 4))))
+# for
 
-class_dict = dict()
-for _ in range(N):
-    class_split = input().split(' ')
-    class_dict[class_split[0]] = [int(class_split[1]), int(class_split[2]), int(class_split[3])]
 
-print(class_dict)
+# print(classroom_time_dict)
 
-all_comb = list(permutations([i for i in range(N)]))
-print(all_comb)
-
+# for class_index in class_permutation[0]:
+#     class_dict
