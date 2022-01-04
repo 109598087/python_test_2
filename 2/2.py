41aac98480284ea2ab970d1bf22cd882ba1da2f5
t109598087@ntut.org.tw <t109598087@ntut.org.tw>
2022-01-05 03:02:03 +0800

    2 ok -輸出

--- 2/2.py
+++ 2/2.py
@@ -12,7 +12,7 @@ def get_all_classroom_comb_list(classroom_list, class_number):
 
 
 def is_time_ok(start_time, end_time, classroom_time_dict):
-    for remove_time in range(start_time, end_time + 1):
+    for remove_time in range(start_time, end_time):
         if remove_time not in classroom_time_dict[classroom]:
             return False
     return True
@@ -26,43 +26,53 @@ classroom_dict = dict()
 for _ in range(M):
     classroom_split = input().split(' ')
     classroom_dict[classroom_split[0]] = int(classroom_split[1])
-print(classroom_dict)
+# print(classroom_dict)
 
 class_dict = dict()
 for _ in range(N):
     class_split = input().split(' ')
     class_dict[class_split[0]] = [int(class_split[1]), int(class_split[2]), int(class_split[3])]
-print(class_dict)
+# print(class_dict)
 
 all_classroom_comb_list = get_all_classroom_comb_list(list(classroom_dict.keys()), len(list(class_dict.keys())))
-print(all_classroom_comb_list)
+# print(all_classroom_comb_list)
 
 all_class_comb_list = list(permutations(list(class_dict.keys())))
-print(all_class_comb_list)
+# print(all_class_comb_list)
 
-# for class_comb in all_class_comb_list:
-#     for classroom_comb in all_classroom_comb_list:
-classroom_time_dict = {key: [i + 1 for i in range(24)] for key in classroom_dict}
+time_list = list()
+all_comb_result_class_classroom_list = list()
+for class_comb in all_class_comb_list:
+    for classroom_comb in all_classroom_comb_list:
+        classroom_time_dict = {key: [i + 1 for i in range(24)] for key in classroom_dict}
+        time = 0
+        result_class_classroom = list()
 
-print(classroom_time_dict)
-print(all_class_comb_list[0])
-print(all_classroom_comb_list[0])
+        for i in range(len(class_comb)):
+            _class = class_comb[i]
+            classroom = classroom_comb[i]
 
-time = 0
-result_class_classroom = list()
-for i in range(len(all_class_comb_list[0])):
-    _class = all_class_comb_list[0][i]
-    classroom = all_classroom_comb_list[0][i]
+            class_people = class_dict[_class][0]
+            start_time = class_dict[_class][1]
+            end_time = class_dict[_class][2]
+            classroom_people = classroom_dict[classroom]
+            # 如果教室有空位，時間也可以 -> 加入
+            if class_people <= classroom_people and is_time_ok(start_time, end_time, classroom_time_dict):
+                time += end_time - start_time
+                result_class_classroom.append([_class, classroom])
+                for remove_time in range(start_time, end_time):
+                    classroom_time_dict[classroom][classroom_time_dict[classroom].index(remove_time)] = 0
+            # print(classroom_time_dict)
+        time_list.append(time)
+        all_comb_result_class_classroom_list.append(result_class_classroom)
+        # print(time)
+        # print(result_class_classroom)
 
-    class_people = class_dict[_class][0]
-    start_time = class_dict[_class][1]
-    end_time = class_dict[_class][2]
-    classroom_people = classroom_dict[classroom]
-    if class_people <= classroom_people and is_time_ok(start_time, end_time, classroom_time_dict):
-        time += end_time - start_time
-        result_class_classroom.append([_class, classroom])
-        for remove_time in range(start_time, end_time + 1):
-            classroom_time_dict[classroom][classroom_time_dict[classroom].index(remove_time)] = 0
-        print(classroom_time_dict)
-print(time)
-print(result_class_classroom)
+print(time_list)
+print(max(time_list))
+print(all_comb_result_class_classroom_list)
+
+max_time = max(time_list)
+for i in range(len(time_list)):
+    if time_list[i] == max_time:
+        print(time_list[i], all_comb_result_class_classroom_list[i])
